4e990e9976c7887580b1c6f921ed22822416b73b
Hui Suan <chunghuisuan@gmail.com>
2015-07-03 22:44:33 +0800

    add basic team analysis

--- main.js
+++ main.js
@@ -23,42 +23,41 @@ angular.module('app', ['ui.bootstrap'])
 
     //NOTE model data
     // $scope.firstTeam = [{
-    //   "data": {
-    //     "ndex": 652,
-    //     "name": "Chesnaught",
-    //     "type": ["fighting", "grass"],
-    //     "moves": ["Rollout", "Spiky Shield", "Mud Shot", "Wood Hammer"],
-    //   },
-    //   "appState": "read",
-    //   "index": 0
-    // }, {
-    //   "data": {
-    //     "ndex": 655,
-    //     "name": "Delphox",
-    //     "type": ["psychic", "fire"],
-    //     "moves": ["Flamethrower", "Sunny Day", "Psychic", "Mystical Fire"],
-    //   },
-    //   "appState": "read",
-    //   "index": 1
-    // }, {
-    //   "data": {
-    //     "ndex": 658,
-    //     "name": "Greninja",
-    //     "type": ["water", "dark"],
-    //     "moves": ["Water Shuriken", "Substitute", "Extrasensory", "Spikes"],
-    //   },
-    //   "appState": "read",
-    //   "index": 2
-    // }, {
-    //   "appState": "blank",
-    //   "index": 3
-    // }];
+    //    "data": {
+    //      "ndex": 652,
+    //      "name": "Chesnaught",
+    //      "type": ["fighting", "grass"],
+    //      "moves": ["Rollout", "Spiky Shield", "Mud Shot", "Wood Hammer"],
+    //    },
+    //    "appState": "read",
+    //    "index": 0
+    //  }, {
+    //    "data": {
+    //      "ndex": 655,
+    //      "name": "Delphox",
+    //      "type": ["psychic", "fire"],
+    //      "moves": ["Flamethrower", "Sunny Day", "Psychic", "Mystical Fire"],
+    //    },
+    //    "appState": "read",
+    //    "index": 1
+    //  }, {
+    //    "data": {
+    //      "ndex": 658,
+    //      "name": "Greninja",
+    //      "type": ["water", "dark"],
+    //      "moves": ["Water Shuriken", "Substitute", "Extrasensory", "Spikes"],
+    //    },
+    //    "appState": "read",
+    //    "index": 2
+    //  }, {
+    //    "appState": "blank",
+    //    "index": 3
+    //  }];
 
     // NOTE this is just for testing and debugging. only use one scope.firstTeam at a time
     $scope.firstTeam = [{
       "appState": "blank"
     }];
-
 
     //NOTE to get array of attacks
     $scope.getAttackList = function(pokemon) {
@@ -70,9 +69,81 @@ angular.module('app', ['ui.bootstrap'])
       return array;
     };
 
+
+    $scope.getTypesArray = function() {
+      var array = [];
+      for (var i = 0; i < $scope.firstTeam.length; i++) {
+        if ($scope.firstTeam[i].appState == "read") {
+          array.push($scope.firstTeam[i].data.type);
+        }
+      }
+      var flatArray = _.compact(_.flatten(array));
+      return flatArray;
+    };
+
+    $scope.countNumArray = function(arr) {
+      var a = [],
+        b = [],
+        prev;
+      arr.sort();
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== prev) {
+          a.push(arr[i]);
+          b.push(1);
+        } else {
+          b[b.length - 1]++;
+        }
+        prev = arr[i];
+      }
+      var compiledArray = [];
+      for (var j = 0; j< a.length; j++) {
+        var tempObject = {
+          "name": a[j],
+          "numCount": b[j]}
+          compiledArray.push(tempObject);
+      }
+      return compiledArray;
+
+      };
+
+
+
+
+    $scope.listOfTypes = ["Bug",
+      "Dark",
+      "Dragon",
+      "Electric",
+      "Fairy",
+      "Fighting",
+      "Fire",
+      "Flying",
+      "Ghost",
+      "Grass",
+      "Ground",
+      "Ice",
+      "Normal",
+      "Poison",
+      "Psychic",
+      "Rock",
+      "Steel",
+      "Water"
+    ];
+
+
     //NOTE Controlling state change functions
+
+    $scope.shouldShowTypes = false;
+    $scope.toggleShowTypes = function() {
+      if ($scope.shouldShowTypes) {
+        $scope.shouldShowTypes = false;
+      }
+      else {
+        $scope.shouldShowTypes = true;
+      }
+    };
+
     $scope.addPokemon = function(pokemon) {
-      if ($scope.firstTeam.length < 6) {
+      if ($scope.firstTeam.length < 7) {
         pokemon.appState = "add";
         var blankSlot = {
           "appState": "blank",
